143e206109742625a9202c4f06666be1d40f6174
somya07 <somya.singhal0797@gmail.com>
2020-07-08 00:19:51 +0530

    somya| header into styled comp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,48 @@
 import React, { useState } from 'react';
-import '../stylesheets/Header.css';
+import styled from 'styled-components';
+
+const MainHeader = styled.div`
+  margin: -8px;
+  background-color: #f60;
+  height: 70px;
+  display: flex;
+  list-style: none;
+  position: fixed;
+  width: -webkit-fill-available;
+  margin-top: -101px;
+`
+const TitleNav = styled.a`
+  color: white;
+  font-size: 25px;
+  text-decoration: none;
+  margin-left: 98px;
+  margin-top: 22px;
+`
+
+const Logo = styled.span`
+  padding: 1px 10px;
+  border: 1px solid #fff;
+  margin-right: 6px;
+  font-family: sans-serif;
+`
+const Dropdown = styled.div`
+  margin-top: 10px;
+  border: 1px solid black;
+  background-color: white;
+  padding: 13px;
+  margin-left: -60px;
+`
+
+const SubHead = styled.div`
+  display: flex;
+  color: white;
+  list-style: none;
+  font-size: 21px;
+  margin-top: 26px;
+  margin-left: 68px;
+`
+
+
 
 
 function Header() {
@@ -11,20 +54,20 @@ function Header() {
   }
 
   return (
-    <div className="Header">
-      <a href="/" className="TitleNav"><span className="logo">Y</span>Hacker News</a>
-      <div className="Subhead">
-        <div className="StoryNav">
+    <MainHeader>
+      <TitleNav href="/"><Logo>Y</Logo>Hacker News</TitleNav>
+      <SubHead>
+        <div style={{marginRight: "20px"}}>
           <div onClick={onClickStories} style={{cursor: "pointer"}}>Stories&nbsp;<i className="fa fa-caret-down"></i></div>
-          <div className="dropdown" style={{display: dropdownvisible ? "none" : "block" }}>
+          <Dropdown style={{display: dropdownvisible ? "none" : "block" }}>
             <div style={{ marginTop: "2px" }}><a href="/" style={{ color: "black", textDecoration: "none" }}>New Stories</a></div>
             <div style={{ marginTop: "10px" }}><a href="/topStories" style={{ color: "black", textDecoration: "none" }}>Top Stories</a></div>
-          </div>
+          </Dropdown>
         </div>
         <div style={{ marginRight: "20px", marginLeft: "20px" }}><a href="/askStories" style={{ color: "white", textDecoration: "none" }}>Ask</a></div>
         <div style={{ marginRight: "20px", marginLeft: "20px" }}><a href="/jobStories" style={{ color: "white", textDecoration: "none" }}>Jobs</a></div>
-      </div>
-    </div>
+      </SubHead>
+    </MainHeader>
   );
 }
 
