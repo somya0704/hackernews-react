f8e9f6983da22881ecbbffbe394f83fe90aa3f78
somya07 <somya.singhal0797@gmail.com>
2020-07-09 01:31:55 +0530

    somya|job_stories hooks

--- src/containers/JobStoriesContainer.jsx
+++ src/containers/JobStoriesContainer.jsx
@@ -1,63 +1,54 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react';
 import {GetJobStories} from '../config/api'
 import Story from '../components/Story'
-import ReactPaginate from 'react-paginate';
+import Paginator from 'react-hooks-paginator';
 import '../stylesheets/StoriesContainer.css'
 
-class JobtoriesContainer extends Component {
-  constructor(props) {
-    super(props)
+function JobtoriesContainer() {
 
-    this.state = {
-      jobstories: [],
-      loading: false,
-      offset: 0,
-      perPage: 25,
-      currentPage: 0
-    }
-  }
+  const pageLimit = 25,
+    [jobstories, setjobstories] = useState([]),
+    [loading, setloading] = useState(false),
+    [offset, setOffset] = useState(0),
+    [currentPage, setCurrentPage] = useState(1);
 
-  receivedJobStories() {
+  useEffect(() => {
     GetJobStories().then((res) => {
-      const slice = res.slice(this.state.offset, this.state.offset + this.state.perPage)
-      this.setState({ jobstories: slice, loading: true, pageCount: Math.ceil(res.length / this.state.perPage) });
+      setjobstories(res.slice(offset, offset + pageLimit));
+      setloading(true);
     }).catch(() => {
-      this.setState({ loading: false });
+      setloading(false);
     });
-  }
+  });
 
-  handlePageClick = (e) => {
-    const selectedPage = e.selected;
-    const offset = selectedPage * this.state.perPage;
-    this.setState({currentPage: selectedPage, offset: offset}, () => {this.receivedJobStories()});
-  };
-
-  componentDidMount() {
-    this.receivedJobStories()
-  }
-
-  render() {
-    return (
-      <>
-        <div className="job_stories" style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", marginTop: "95px" }}>
-          {this.state.jobstories.map(storyId =>(<Story key={storyId} storyId={storyId} />))}
-        </div>
-        <div>
-          <ReactPaginate
-            previousLabel={"prev"}
-            nextLabel={"next"}
-            breakLabel={"..."}
-            breakClassName={"break-me"}
-            pageCount={this.state.pageCount}
-            marginPagesDisplayed={2}
-            pageRangeDisplayed={5}
-            onPageChange={this.handlePageClick}
-            containerClassName={"pagination"}
-            subContainerClassName={"pages pagination"}
-            activeClassName={"active"}/>
-        </div>
-      </>
-    );
-  }
+  return (
+    <>
+      { loading &&
+        <>
+          <div className="job_stories" style={{ display: "flex", flexWrap: "wrap", justifyContent: "center", marginTop: "95px" }}>
+            {jobstories.map(storyId =>(<Story key={storyId} storyId={storyId} />))}
+          </div>
+          <div>
+            <Paginator
+            totalRecords={jobstories.length}
+            pageLimit={pageLimit}
+            pageNeighbours={1}
+            setOffset={setOffset}
+            currentPage={currentPage}
+            setCurrentPage={setCurrentPage}
+            pageActiveClass={"active"}
+            pageContainerClass={"react-hooks-paginator"}
+            pagePrevText={"prev"}
+            pageNextText={"next"}
+            pageItemClass={"page-item"}
+            pageLinkClass={"page-link"}
+            pagePrevClass={"page-link"}
+            pageNextClass={"next-link"}
+          />
+          </div>
+        </>
+      }
+    </>
+  );
 }
 export default JobtoriesContainer;
